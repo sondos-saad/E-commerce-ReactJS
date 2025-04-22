6753b68b9f39c4f6bf0cd3a75704ec010b2bb319
sondos-saad <sondossaad812@gmail.com>
2025-04-22 17:34:59 +0200

    header

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { MdLocalShipping ,MdLogin } from "react-icons/md";
-import { FaSearch } from "react-icons/fa";
+import { MdLocalShipping ,MdLogin ,MdLogout} from "react-icons/md";
+import { FaSearch ,FaUserAlt} from "react-icons/fa";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from 'react-router-dom';
 
 function Nav() {
     const { loginWithRedirect } = useAuth0();
+    const { logout , user, isAuthenticated} = useAuth0();
   return (
     <div className='w-full'>
         <div className=' flex p-[10px] bg-amber-400'>
@@ -16,19 +18,71 @@ function Nav() {
             </div>
         </div>
         <div className='w-[80%] mx-auto flex items-center justify-between py-4'>
-            <h1 className='text-5xl font-bold text-amber-900'>LoGo</h1>
+            <h1 className='text-5xl font-bold text-black'>LoGo</h1>
             <div className='flex items-center gap-1'>
                 <input type='text' value='' placeholder='Search' className='border-[1px] rounded-xl p-2'/>
-                <button className='bg-amber-900 p-2 rounded-xl text-white w-[50px] flex items-center justify-center h-[40px]'><FaSearch /></button>
+                <button className='bg-amber-400 p-2 rounded-xl text-black w-[50px] flex items-center justify-center h-[40px]'><FaSearch /></button>
             </div>
-            <div className='flex items-center justify-center gap-2'>
-                <div className='font-bold text-xl'>
-                    <MdLogin />
+            {
+                isAuthenticated ?   
+                // logout button
+                    <div className='flex items-center justify-center gap-2'>
+                        <div className='font-bold text-xl'>
+                            <MdLogout />
+                        </div>
+                        <div className='font-bold text-lg bg-amber-400 p-2 rounded-xl text-black'>
+                            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout</button>
+                        </div>
+                    </div>
+                    :
+                    // login button
+                    <div className='flex items-center justify-center gap-2'>
+                        <div className='font-bold text-xl'>
+                            <MdLogin />
+                        </div>
+                        <div className='font-bold text-lg bg-amber-400 p-2 rounded-xl text-black'>
+                            <button onClick={() => loginWithRedirect()}>Login</button>
+                        </div>
+                    </div>
+            }
+        </div>
+        <div className=' bg-black text-white py-2'>
+           <div className='w-[80%] mx-auto flex items-center justify-between'>
+            <div className='flex items-center gap-[.5rem]'>
+                        {
+                            isAuthenticated ? <>
+                                <div className=' text-amber-400 p-2 rounded-xl'>
+                                    <FaUserAlt />
+                                </div>
+                                <div>
+                                    <h2 className='text-xs'>{user.name}</h2>
+                                    <p className='text-xs'>{user.email}</p>
+                                </div>
+                            </>
+                            :
+                            <>
+                                <div className='text-amber-400 p-2 rounded-xl'>
+                                    <FaUserAlt />
+                                </div>
+                                <div>
+                                    <p className='text-xs'>Please Login</p>
+                                </div>
+                            </>
+                        }
                 </div>
-                <div className='font-bold text-lg bg-amber-900 p-2 rounded-xl text-white'>
-                   <button onClick={() => loginWithRedirect()}>Login</button>
+                <div >
+                    <ul className='flex items-center gap-[1rem] font-bold'>
+                        <li><Link to='/' className='hover:text-amber-400'>Home</Link></li>
+                        <li><Link to='/shop' className='hover:text-amber-400'>Shop</Link></li>
+                        <li><Link to='/collection' className='hover:text-amber-400'>Collection</Link></li>
+                        <li><Link to='/about' className='hover:text-amber-400'>About</Link></li>
+                        <li><Link to='/contact' className='hover:text-amber-400'>Contact</Link></li>
+                    </ul>
                 </div>
-            </div>
+                <div className='bg-amber-400 p-2 rounded-xl text-black font-bold'>
+                    <p>flat 10% over all iphone</p>
+                </div>
+           </div>
         </div>
     </div>
   )
