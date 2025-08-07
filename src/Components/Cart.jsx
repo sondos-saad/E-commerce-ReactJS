e392ebd5d34b5611008dab7da58548d54761e216
sondos-saad <sondossaad812@gmail.com>
2025-08-07 19:07:15 +0300

    cartpage

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,20 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import photo from '../assets/laptop1.jpg';
 
 function Cart({cart}) {
   return (
     <section className='cart w-[80%] mx-auto py-2'>
-        <h3 className='text-3xl font-bold uppercase tracking-wide'>#cart</h3>
+        <h3 className='text-3xl font-bold uppercase tracking-wide'># cart</h3>
         {
             cart.length === 0 && 
             <>
             <div className='empty_cart'>
-                <h2>Your Shopping Cart Is Empty</h2>
-                <button>Shop Now</button>
+                <h2 className='text-center uppercase text-xl tracking-wide'>Your Shopping Cart Is Empty</h2>
+                <Link to={'/shop'}>
+                    <div className='flex justify-center mt-[2rem]'>
+                    <button className='bg-amber-500 p-4 rounded-xl font-bold border-amber-500 border-[1px] hover:bg-transparent hover:border-amber-500 duration-300 cursor-pointer'>Shop Now</button>
+                </div>
+                </Link>
             </div>
             </>
         }
         <div className='container'>
-            
+            {
+                cart.map((curElm) => {
+                    return(
+                        <>
+                        <div className='box'>
+                            <div className='img_box'>
+                                <img src={curElm.img} alt='photo' className='rounded-2xl'/>
+                            </div>
+                            <div className='details'>
+                                <h4>{curElm.cat}</h4>
+                                <h3>{curElm.name}</h3>
+                                <p>Price: ${curElm.price}</p>
+                                <div className='quantity'>
+                                    <button>+</button>
+                                    <input type='number' value={curElm.qty}></input>
+                                    <button>-</button>
+                                </div>
+                            </div>
+                        </div>
+                        </>
+                    )
+                })
+            }
         </div>
     </section>
   )
