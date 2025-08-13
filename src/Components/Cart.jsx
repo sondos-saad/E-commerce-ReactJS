57c30f407accf2287d02e928aa98d85b0d55c126
sondos-saad <sondossaad812@gmail.com>
2025-08-13 19:37:44 +0300

    .

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import photo from '../assets/laptop1.jpg';
-import { FaWindowClose } from 'react-icons/fa';
 import { AiOutlineClose } from 'react-icons/ai';
 
 
@@ -22,31 +20,30 @@ function Cart({cart}) {
             </div>
             </>
         }
-        <div className='container mt-[2rem]'>
+        <div className='container my-[2rem] bg-gray-200 p-2 rounded-xl'>
             {
                 cart.map((curElm) => {
                     return(
                         <>
                         <div className='box flex items-center gap-[2rem]'>
-                            <div className='img_box w-[200px]'>
+                            <div className='img_box w-[20%]'>
                                 <img src={curElm.img} alt='photo' className='rounded-2xl'/>
                             </div>
-                            <div className='details'>
+                            <div className='details flex justify-between items-center gap-[2rem] w-[75%]'>
                                 <div className='info'>
                                     <h4 className='text-amber-500 font-bold text-lg'>{curElm.cat}</h4>
                                     <h3 className='text-2xl font-bold tracking-wide'>{curElm.name}</h3>
                                     <p className='my-2'>Price: ${curElm.price}</p>
+                                    <p className='my-2 pt-2 border-t-[1px] text-xl'>Total: ${curElm.price * curElm.qty}</p>
                                 </div>
                                 <div className='quantity flex gap-[1rem] items-center mb-4'>
-                                    <button className='text-xl'>+</button>
+                                    <button className='font-bold bg-amber-500 w-[40px] h-[40px] rounded-full cursor-pointer'>+</button>
                                     <input type='number' value={curElm.qty} className='border rounded-2xl p-1 w-[50px] text-center '></input>
-                                    <button className='text-lg'>-</button>
+                                    <button className='font-bold bg-amber-500 w-[40px] h-[40px] rounded-full cursor-pointer'>-</button>
                                 </div>
-                                <p className='my-2 pt-2 border-t-[1px] text-xl'>Total: ${curElm.price * curElm.qty}</p>
+                                
                                 <div className='icon'>
-                                    <li>
-                                        <AiOutlineClose/>
-                                    </li>
+                                        <AiOutlineClose className='font-bold text-xl cursor-pointer'/>
                                 </div>
                             </div>
                         </div>
